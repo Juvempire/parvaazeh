64b389507f85d1d0b079b78e290c0be473436beb
lvlojtaba.sh <33198584+Juvempire@users.noreply.github.com>
2025-09-28 11:23:40 +0330

    fix bug

--- view_leads.php
+++ view_leads.php
@@ -31,14 +31,6 @@ echo '<!DOCTYPE html>
         tr:nth-child(even) { background-color: #f9f9f9; }
         .export-link { display: inline-block; padding: 10px 15px; background-color: #28a745; color: white; text-decoration: none; border-radius: 5px; margin-bottom: 20px; }
     </style>
-<!-- Event snippet for Page view conversion page -->
-  	<script>
-		gtag('event', 'conversion', {
-			'send_to': 'AW-17603536866/HliqCL_X4KIbEOLPgspB',
-			'value': 1.0,
-			'currency': 'USD'
-		});
-	</script>
 
   </head>
 <body>
